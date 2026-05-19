3f131f0eb084525b4d2e2bab0ca031e98031cc6f
Alexandr Kuchin <alexdedaev@gmail.com>
2026-05-19 12:56:26 +0200

    Preparando release 1.0.0

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,19 +1,23 @@
 
 public class Calculadora {
+	
+	//Añado metodo sumar
 	public int sumar(int a, int b) {
 		return a + b;
 	}
 	
+	//Añado metodo restar
 	public int restar(int a, int b) {
 		return a - b;
 	}
 	
+	//Añado metodo multiplicar
 	public int multiplicar(int a, int b) {
 		return a * b;
 	}
 	
+	//Añado metodo dividir
 	public double dividir(int a, int b) {
 		return (double) a / b;
 	}
-
 }
